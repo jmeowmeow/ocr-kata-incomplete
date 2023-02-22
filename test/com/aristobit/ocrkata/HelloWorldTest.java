aa4deae262aa939b800978da1b7f419cd0f506f3
jeffrey.miller <jeffrey.miller@gmail.com>
2023-02-22 11:18:56 -0800

    JUnit 4 hello-world test

---
+++ test/com/aristobit/ocrkata/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package com.aristobit.ocrkata;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Get the lights on with JUnit 4
+ */
+public class HelloWorldTest {
+
+	@Test
+	public void lightsAreOn() {
+		assertTrue("Hello world should contain \"world\"", "Hello world".contains("world"));
+	}
+
+	@Test
+	public void seeItFailThenFix() {
+		assertFalse("Hello world should not contain \"weird\"", "Hello world".contains("weird"));		
+	}
+}
