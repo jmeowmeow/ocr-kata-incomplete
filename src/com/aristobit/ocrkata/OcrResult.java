0867b8404a9e32dbd294057e87bf07952df4e2eb
jeffrey.miller <jeffrey.miller@gmail.com>
2023-02-22 13:43:18 -0800

    OcrResult has List<String> of acct#

--- src/com/aristobit/ocrkata/OcrResult.java
+++ src/com/aristobit/ocrkata/OcrResult.java
@@ -1,9 +1,15 @@
 package com.aristobit.ocrkata;
 
+import java.util.List;
+
 public class OcrResult {
 
 	public int getSize() {
 		return 0;
+	}
+
+	public List<String> getParsedAccounts() {
+		return List.of("000000000");
 	}
 
 }
--- test/com/aristobit/ocrkata/OcrReaderTests.java
+++ test/com/aristobit/ocrkata/OcrReaderTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -19,11 +20,16 @@ public class OcrReaderTests {
 		assertEquals("no account numbers expected", 0, result.getSize());
 	}
 	
-	private void assertOcrResultsFromFile(String filename, int expectedAccountCount) {
+	private void assertOcrCountFromFile(String filename, int expectedAccountCount) {
+		OcrResult result = getOcrResultForFilename(filename);
+		assertEquals("count of account numbers from ["+filename+"]", expectedAccountCount, result.getSize());
+	}
+
+	private OcrResult getOcrResultForFilename(String filename) {
 		BufferedReader br = getBufferedFileReaderFrom(filename);
 		reader = new OcrReader(br);
 		OcrResult result = reader.getResult();
-		assertEquals("count of account numbers from ["+filename+"]", expectedAccountCount, result.getSize());
+		return result;
 	}
 	
 	private BufferedReader getBufferedFileReaderFrom(String filename) {
@@ -33,7 +39,21 @@ public class OcrReaderTests {
 
 	@Test
 	public void testEmptyResultsFromFile() {
-		assertOcrResultsFromFile("/resources/empty.txt", 0);
+		assertOcrCountFromFile("/resources/empty.txt", 0);
+	}
+
+	@Test
+	public void testFindAccount000000000() {
+		OcrResult result = getOcrResultForFilename("/resources/000000000.txt");
+		List<String> parsedAccounts = result.getParsedAccounts();
+		assertEquals("first acct#", "000000000", parsedAccounts.get(0));
+	}
+
+	@Test
+	public void testFindAccount123456789() {
+		OcrResult result = getOcrResultForFilename("/resources/123456789.txt");
+		List<String> parsedAccounts = result.getParsedAccounts();
+		assertEquals("first acct#", "123456789", parsedAccounts.get(0));
 	}
 
 	// Pending numeric parsing of an input record
