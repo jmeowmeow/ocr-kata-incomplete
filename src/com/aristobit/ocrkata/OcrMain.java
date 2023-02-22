34458c1dbad6762f62d292fb999935af359a1ae1
jeffrey.miller <jeffrey.miller@gmail.com>
2023-02-22 12:57:10 -0800

    run one or more input files from command line

--- src/com/aristobit/ocrkata/OcrMain.java
+++ src/com/aristobit/ocrkata/OcrMain.java
@@ -3,28 +3,38 @@ package com.aristobit.ocrkata;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * A command-line runner for OcrReader.
+ */
 public class OcrMain {
 
 	public static void main(String[] args) throws IOException {
 		if (args == null || args.length < 1) {
-			System.out.println("OcrReader needs an input file name to process.");
+			System.out.println("OcrReader needs at least one input file name to process.");
 			return;
 		} else {
-			File file = new File(args[0]);
-			System.out.println("Input OCR file: ["+ file.getPath() + "]");
-			if (!file.exists()) {
-				System.out.println("Can't find file.");
-				return;
+			for (String filename : args) {
+				processOneFilename(filename);
 			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			OcrReader ocrReader = new OcrReader(br);
-			OcrResult result = ocrReader.getResult();
-			System.out.println("The OcrReader found "+result.getSize()+" account numbers.");
 		}
 		
+	}
+
+	private static void processOneFilename(String filename) throws FileNotFoundException {
+		File file = new File(filename);
+		System.out.println("Input OCR file: ["+ file.getPath() + "]");
+		if (!file.exists()) {
+			System.out.println("Can't find file.");
+			return;
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		OcrReader ocrReader = new OcrReader(br);
+		OcrResult result = ocrReader.getResult();
+		System.out.println("The OcrReader found "+result.getSize()+" account numbers.");
 	}
 
 }
